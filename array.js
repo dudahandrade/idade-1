6dcea3ff9df9e9d1ba0ed23070ba8fe8c705a620
dudahandrade <130777294+dudahandrade@users.noreply.github.com>
2024-06-11 09:00:38 -0300

    Update array.js

--- array.js
+++ array.js
@@ -1,5 +1,10 @@
-const produtos = ["arroz","feijão","chocolate","bolacha","macarrão","sabonete","miojo","refrigerante","biscoito","pera"]
-console.log(produtos);
-console.log(produtos(3));
-console.log(produtos.length);
-produtos=[]
+lista1 = ["Eduarda", "Maria", "Monica"]
+lista2 = ["Eduarda", "Maria", "Monica"]
+listacompleta = lista1.concat(lista2)
+console.log (listacompleta)
+
+const dado = [["Marcos", "Rafaela", "Maria"], [8, 8.5, 9]];
+console.log("nome:" +dado[0][1] + "média: + dado[1] [1]");
+
+const dado2 = [["Marcos", "Rafaela", "Maria"], [[3,9,7],[5,8,9],[8,9,7]]];
+console.log("nome:" +dado[0][1] + "média:" + dado2[1][1][0]);
