75d64342bc608008b7c8cfb23121083586d570c8
dudahandrade <130777294+dudahandrade@users.noreply.github.com>
2024-08-05 12:33:09 -0300

    Create objetoemobjeto.js

---
+++ objetoemobjeto.js
@@ -0,0 +1,19 @@
+const cliente = {
+    nome: "Eduarda", 
+    idade: 40,
+    email: "JP@tech.com", 
+    telefone: ["41 98888-2222", "41 97777-3333"]
+}
+
+cliente.enderenco = {
+    rua: 'Sete de setembro', 
+    numero: 954, 
+    apartamento: true, 
+    complemento: "ap 25"
+}
+
+
+
+console.log(cliente)
+
+console.log(cliente.endereco)
