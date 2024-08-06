54c1b57fc3cc780c5231febacc0af8a3992864b1
dudahandrade <130777294+dudahandrade@users.noreply.github.com>
2024-08-06 09:08:30 -0300

    Create listtadeobjeto.js

---
+++ listtadeobjeto.js
@@ -0,0 +1,28 @@
+const Cliente1 ={
+    nome:"Rafa",
+    idade: 25,
+}
+
+Cliente.endereco ={
+                    rua: "Brasil",
+                    numero: 500,
+}
+
+const Cliente2 = {
+    nome: "Duda", 
+    idade: 16,
+    endereco: {
+        rua: "Brasil",
+        nuemro: 500
+    }
+}
+
+console.log(Cliente1)
+console.log(Cliente2)
+
+
+const lista = [" banana", "morango",]
+const endereco =[{rua: " "}, {rua: " "}]
+
+endereco[0].rua
+
