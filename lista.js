a566135d26c34d35f2469e7b6032bf7b43fe4c00
dudahandrade <130777294+dudahandrade@users.noreply.github.com>
2024-08-19 09:03:02 -0300

    Create lista.js

---
+++ lista.js
@@ -0,0 +1,7 @@
+const produtos = ["laranja","morango","uva","maça","macarrão","banana",]
+
+const frutasMaiusculas = [];
+for (let i= 0; i<frutas.length;i++){
+    frutas.push(frutas[i].toUpperCase())
+}
+console.log(frutasMaiusculas);
